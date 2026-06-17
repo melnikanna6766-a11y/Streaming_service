f26ca3f13e1ad4cbda74be9ae330c9a2f22a1a6d
Anna <melana0607061@gmail.com>
2026-06-17 20:33:15 +0300

    Initial commit

---
+++ src/test/java/com/github/melnikanna6766a11y/streaming_service/StreamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.melnikanna6766a11y.streaming_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
